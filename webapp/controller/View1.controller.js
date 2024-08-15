ea79123ba6a299219220393b7ab33ac380959863
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2024-08-15 02:26:12 +0000

    new changes added

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("trainingoncapmui5.controller.View1", {
+        onInit: function () {
+
+        }
+    });
+});
